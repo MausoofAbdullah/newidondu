1840a32fe490315000b00d6c76a576690c0481b6
MausoofAbdullah <mausoofabdullah@gmail.com>
2024-02-27 12:21:14 +0530

    multere cloud storeage relmoved

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import createError from "http-errors"
+// import createError from "http-errors"
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
